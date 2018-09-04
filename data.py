a2e972c1b58922c863395eec84ccabf693e1028c
ybh1998 <ybh1998@163.com>
2018-09-04 19:54:07 +0800

    change of data.py

--- data.py
+++ data.py
@@ -7,7 +7,14 @@ from match_core import match
 from match_interface import save_match_log
 from time import perf_counter as pf
 
-def worker(id, net, data_queue):
+def getFeatureMap(stat, me):
+    meFields = stat['now']['fields'] == me
+    enemyFields = stat['now']['fields'] == 3 - me
+    meBands = stat['now']['bands'] == me
+    enemyBands = stat['now']['bands'] == 3 - me
+    mePosition = 
+
+def worker(id, net, data_queue, save = False):
     class player1:
         def play(self, stat, storage):
             return 'r'
@@ -20,12 +27,20 @@ def worker(id, net, data_queue):
     res = match((player1(), player2()))
     t2 = pf()
     print(t2 - t1)
-    save_match_log(res, 'saves/1.zlog')
+    
+    if save:
+        save_match_log(res, 'saves/1.zlog')
+        
+def start():
+    pass
+
+def getData():
+    pass
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
     q = mp.Queue()
-    p = mp.Process(target=worker, args=(1, 0, q))
+    p = mp.Process(target=worker, args=(1, 0, q, True))
     p.start()
     p.join()
     
